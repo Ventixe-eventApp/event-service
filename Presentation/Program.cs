12db0fb6647917e69b1638294718fb7536b444d7
Felicia Dahl <feliciadahl01@gmail.com>
2025-05-30 15:09:44 +0200

    Swagger for dev

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -22,13 +22,16 @@ builder.Services.AddScoped<IEventPackageRepository, EventPackageRepository>();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI(c =>
+if (app.Environment.IsDevelopment())
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Event API V1");
-    c.RoutePrefix = string.Empty;
-});
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Event API V1");
+        c.RoutePrefix = string.Empty;
+    });
 
+}
 
 app.MapOpenApi();
 app.UseHttpsRedirection();
