192188ad6ea660e2fee4340aaf0f030eddb57bc3
Felicia Dahl <feliciadahl01@gmail.com>
2026-01-04 15:26:02 +0100

    refactor project file for better docker support

--- Presentation/Data/Entities/PackageEntity.cs
+++ Presentation/Data/Entities/PackageEntity.cs
@@ -7,11 +7,16 @@ public class PackageEntity
 {
     [Key]
     public int Id { get; set; }
+
     public string PackageName { get; set; } = null!;
+
     public string SeactionType { get; set; } = null!;
+
     public string? Description { get; set; }
+
     [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
+
     public int AvailableQuantity { get; set; }
 
 }
